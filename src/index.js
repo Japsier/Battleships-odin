cee69ad91ea60ff5707e93ac0b91617a43904d75
JapSir <japsircode@gmail.com>
2022-11-15 15:54:40 +0100

    make ship and ggameboard objects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,89 @@
 import "./style.css"
 
+export const ship = (lengthShip) => {
+    let length = lengthShip
+    let hits = 0
+    let hit = () => {
+        hits++
+    }
+    let isSunk = () => (length - hits == 0) ? true : false
+
+    return {isSunk, hit}
+}
+
+export const gameBoard = () => {
+
+    let gameBoardArray = []
+    const tile = (coordinates) => {
+        let location = coordinates
+        let hasShip = false
+        let shipID = null
+        let hasHit = false
+
+        return {location, hasShip, shipID, hasHit}
+    }
+    for (let i = 0; i < 100; i++) {
+        gameBoardArray.push(tile(i))
+    }
+
+    let placeShip = (coordinates, lengthShip) => {
+        let check = coordinates + lengthShip
+        let valid = false
+
+        //check if ship fits on vertical line
+        let temp1 = 0
+        let temp2 = 10
+        for(let i = 0; i < 10; i++) {
+            if(temp1 < check && temp2 > check && temp1 < coordinates && temp2 > coordinates) {
+                valid = true
+            }
+            temp1 = temp1 + 10
+            temp2 = temp2 + 10
+        }
+
+        //checks if ship is not on top of other ship 
+        for (let i = coordinates; i < check; i++) {
+            if(gameBoardArray[i].hasShip) {
+                valid = false
+            }
+        }
+
+        if (!valid) {
+            return "invalid"
+        }
+
+        //create ship and put in onto board
+        let shipType = ship(lengthShip)
+        for (let i = coordinates; i < check; i++) {
+            gameBoardArray[i].hasShip = true
+            gameBoardArray[i].shipID = shipType
+        }
+        return "succes"
+        
+    }
+    const isWinner = () => {
+        for(let i = 0; i < 100; i++) {
+            if (gameBoardArray[i].hasShip) {
+                if (!gameBoardArray[i].hasHit) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+    const recieveAttack = (coordinates) => {
+        if(gameBoardArray[coordinates].hasHit) {
+            return "invalid"
+        }
+        gameBoardArray[coordinates].hasHit = true
+        if (gameBoardArray[coordinates].hasShip == true) {
+            gameBoardArray[coordinates].shipID.hit()
+            return "hit"
+        }
+        return "missed"
+    }
+
+    return {placeShip, recieveAttack}
+}
 
 
-
-module.exports = *
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,1 +1,34 @@
-import "../index"
+import * as myObj from "../index"
+
+
+test("ship sunk after enough hits", () => {
+    let hits = 4
+    let testShip = myObj.ship(4)
+    for(let i = 0; i < hits; i++) {
+     testShip.hit()   
+    }
+    expect(testShip.isSunk()).toBe(true)
+})
+
+test("ship placement", () => {
+    let board = myObj.gameBoard()
+    expect(board.placeShip(26, 3)).toBe("succes")
+})
+test("attack hit", () => {
+    let board = myObj.gameBoard()
+    board.placeShip(26, 3)
+    expect(board.recieveAttack(28)).toBe("hit")
+})
+test("attack miss", () => {
+    let board = myObj.gameBoard()
+    board.placeShip(26, 3)
+    board.placeShip(41, 4)
+    expect(board.recieveAttack(32)).toBe("missed")
+})
+test("attack invalid", () => {
+    let board = myObj.gameBoard()
+    board.placeShip(26, 3)
+    board.placeShip(41, 4)
+    board.recieveAttack(32)
+    expect(board.recieveAttack(32)).toBe("invalid")
+})
