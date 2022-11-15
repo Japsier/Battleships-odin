b6fa541544eb216f434373598d4eeecfc5181efc
JapSir <japsircode@gmail.com>
2022-11-15 13:17:07 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+        {
+            test: /\.jsx?$/,
+            exclude: ['node_modules'],
+            use: ['babel-loader'],
+          },
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.gif$/,
+            type: 'asset/inline',
+          },
+          {
+            test: /\.(ttf|eot|svg)$/,
+            type: 'asset/resource',
+          },
+    ],
+  },
+};
